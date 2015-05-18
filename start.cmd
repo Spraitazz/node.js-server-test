87de7fc75a0eb58e923c690a6f10d7624b999196
Jonas <spraitazz@gmail.com>
2015-05-18 13:47:41 +0100

    added cmd launcher

---
+++ start.cmd
@@ -0,0 +1,3 @@
+node staticServer.js
+
+
--- staticServer.js
+++ staticServer.js
@@ -1,4 +1,27 @@
 var express = require("express");
+var readline = require("readline");
 var app = express();
 app.use(express.static("Server"));
-app.listen(3000);
+var port;
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt("specify a port \n");
+rl.prompt();
+rl.on("line", function(line) {
+if (!isNaN(line)) {
+if (line > 0 && line < 65000) {
+port = line;
+app.listen(port);
+console.log("Server started successfully on port " + port);
+rl.close();
+}
+else {
+console.log("ERROR. incorrect value for port (0 to 65000 only)");
+}
+}
+else {
+console.log("ERROR. enter integer value");
+}
+
+});
+
+
