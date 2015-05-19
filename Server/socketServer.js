049dc80d3e4fe1a998b68dea9e55172172eb309d
Jonas <spraitazz@gmail.com>
2015-05-19 14:43:16 +0100

    online list, more message types

--- Server/socketServer.js
+++ Server/socketServer.js
@@ -9,29 +9,26 @@ this.socket = socket;
 this.username = "";
 };
 
+function broadcast (msg) {
+wss.clients.forEach(function (client) {
+    client.send(msg);
+  });
+}
+
 //socket connecting
 wss.on("connection", function connection(ws) {
 //a single websocket starts here
 var gotName = false;
-var thisSocket = thisSocket = new ClientSocket(ws);
+var thisSocket = new ClientSocket(ws);
 
 ws.on("open", function open() {
-//send online list on opening
-var response = [];
-response.type = "online";
-var onlineList = [];
-clientSockets.forEach(function(value, index) {
-onlineList.push(value);
-});
-response.body = onlineList;
-ws.send(JSON.stringify(response));
-clientSockets.push(thisSocket);
+//socket doesnt exist until it has a name
 });
 
 ws.on("close", function close() {
 //remove websocket from sockets
 for (var i = 0; i < clientSockets.length; i++) {
-if (clientSockets[i].socket == ws) {
+if (clientSockets[i].socket === ws) {
 clientSockets.splice(i, 1);
 }
 }
@@ -41,32 +38,52 @@ clientSockets.splice(i, 1);
 //message received
 ws.on("message", function incoming(message) {
 
-var response = [];
+var response = {};
 
-if (gotName == false) {
+if (gotName === false) {
 //name not chosen
 response.type = "username";
 var nameTaken = false;
 clientSockets.every(function(currentValue, index) {
 //loop through sockets to check if name exists
-if (currentValue.username == message) {
+if (currentValue.username === message) {
 //name exists, inform client
 nameTaken = true;
 response.status = "nt";
+ws.send(JSON.stringify(response));
 return false;
 }
 else {
 return true;
 }
 });
-if (nameTaken == false) {
-//name doesn't exist, set name as message
+if (nameTaken === false) {
+//name doesn't exist, set name as message and SEND
 thisSocket.username = message;
+clientSockets.push(thisSocket);
 gotName = true;
 response.status = "nc";
 response.username = message;
-}
 ws.send(JSON.stringify(response));
+
+//broadcast new connection to others
+var newConnection = {};
+newConnection.type = "connected";
+newConnection.username = message;
+broadcast(JSON.stringify(newConnection));
+
+//send online list to new user
+var online = {};
+online.type = "online";
+var onlineList = [];
+clientSockets.forEach(function(value, index) {
+onlineList.push(value.username);
+});
+online.body = onlineList;
+ws.send(JSON.stringify(online));
+
+
+}
 }
 
 // name chosen
